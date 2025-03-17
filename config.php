b406170a62b70ea9b1d394e29a8a87bed72a0e27
unknown <nanda30rizky@gmail.com>
2025-03-17 08:29:16 +0700

    feat: init

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$databaseHost = 'localhost';
+$databaseName = 'wad_handson';
+$databaseUsername = 'root';
+$databasePassword = '';
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
